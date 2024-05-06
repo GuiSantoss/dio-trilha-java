17ce895647e36d160fe65b946520eb13931dcdc0
Guilherme Santos <gh.vieirasantos@gmail.com>
2024-05-06 16:59:10 -0300

    SmartTv

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,21 @@
+public class Operadores {
+    public static void main(String[] args) {
+        boolean condicao1 = true;
+
+        boolean condicao2 = true;
+
+        if (condicao1 && condicao2) {
+            System.out.println("as duas condições sao verdadeiras");
+            
+        }
+
+
+        if (condicao1 || condicao2) {
+            System.out.println("Uma das condiçies é verdadeira");
+            
+        }
+
+        System.out.println("fIm");
+    }
+    
+}
