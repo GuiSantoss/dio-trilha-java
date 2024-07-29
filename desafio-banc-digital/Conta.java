100077204c4bc6daad7ca1221cf6f0ac9b29194e
Guilherme Santos <gh.vieirasantos@gmail.com>
2024-07-29 13:19:45 -0300

    Desafio Banco Digital

---
+++ desafio-banc-digital/Conta.java
@@ -0,0 +1,46 @@
+public abstract class Conta {
+    private static int SEQUENCIAL = 1;
+    private static final int AGENCIA_PADRAO = 1;
+
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+
+    public Conta() {
+        this.agencia = AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+    }
+
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    public void sacar(double valor) {
+        saldo -= valor;
+    }
+
+    public void transferir(double valor, Conta contaDestino) {
+        this.sacar(valor);
+        contaDestino.depositar(valor);
+    }
+
+    // Métodos get e set para agencia, numero e saldo
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    protected void imprimirInfosComuns() {
+        System.out.println(String.format("Agencia: %d", this.agencia));
+        System.out.println(String.format("Numero: %d", this.numero));
+        System.out.println(String.format("Saldo: %.2f", this.saldo));
+    }
+}
